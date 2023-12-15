ec206409a0c87b85db22e33b90f93289c965eea6
NeedThisAccount <36775976+Retardeded@users.noreply.github.com>
2023-12-15 14:36:20 +0100

    removing angular material/adjusting/simplifying

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,11 @@
 // header.component.ts
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
